ee2d48d99615ad6de0a4232d2fa95f1a480e6722
NetroxTTV <147041723+NetroxTTV@users.noreply.github.com>
2024-02-28 11:47:26 +0100

    flip

--- Assets/Lucas/Scripts/PlayerMovement.cs
+++ Assets/Lucas/Scripts/PlayerMovement.cs
@@ -13,12 +13,14 @@ public class PlayerMovement : MonoBehaviour
     public float x;
     public float y;
     public float z;
+    public SpriteRenderer spriteRenderer;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -26,8 +28,9 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 currentVelocity = new Vector2(0, body.velocity.y);
         bool isRunning = false;
-            
-        if(Input.GetKey(KeyCode.D))
+        spriteRenderer.flipX = false;
+
+        if (Input.GetKey(KeyCode.D))
         {
             currentVelocity += new Vector2(5, 0);
             isRunning = true;
@@ -36,6 +39,7 @@ public class PlayerMovement : MonoBehaviour
         {
             currentVelocity += new Vector2(-5, 0);
             isRunning = true;
+            spriteRenderer.flipX = true;
         }
 
         animator.SetBool("isRunning", isRunning);
@@ -62,6 +66,8 @@ public class PlayerMovement : MonoBehaviour
             Vector3 LoadPosition = new Vector3(-7, -1, 0);
             transform.position = LoadPosition;
         }
+
+        
     }
 
 }
