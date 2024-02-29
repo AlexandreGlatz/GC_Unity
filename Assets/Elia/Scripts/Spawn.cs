a9e52e59c6e86dd75455457f388b1621c70798c3
Kaktousse <elia.maillardsalin@gmail.com>
2024-02-29 10:56:47 +0100

    spawn carré

--- Assets/Elia/Scripts/Spawn.cs
+++ Assets/Elia/Scripts/Spawn.cs
@@ -1,45 +1,52 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Spawn : MonoBehaviour
 {
 
-    public GameObject spawn;
-    private DateTime Datetimebegin;
-    private bool spawned_item;
+    public GameObject spawner;
+    public GameObject follow;
+    public GameObject objectToSpawn;
+    private float halflength,startpos;
+    private float distance;
+    private Vector2[] allitems;
 
     // Start is called before the first frame update
 
 
     void Start()
     {
-        Datetimebegin = DateTime.Now;
-        spawned_item = false;
+
+        Camera cam = Camera.main;
+        halflength = (cam.orthographicSize * cam.aspect);
+        startpos = follow.transform.position.x;
+        distance = 5;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (spawned_item == false)
+        if (follow.transform.position.x - startpos > distance) 
         {
-            SpawnObject(spawned_item);
+            SpawnItem();
+            startpos += distance; 
         }
+
     }
 
-    void SpawnObject(bool spawned)
+    void SpawnItem()
     {
-        DateTime Datecycle = DateTime.Now;
+        Instantiate(objectToSpawn);
+        objectToSpawn.transform.position = spawner.transform.position;
+        allitems = allitems.Append(new Vector2(objectToSpawn.transform.position.x,objectToSpawn.transform.position.y)).ToArray();
+        Console.WriteLine(allitems);
+    }
 
-        long elapsedticks = Datetimebegin.Ticks - Datecycle.Ticks;
-        TimeSpan elapsedtime = new TimeSpan(elapsedticks);
-        if (elapsedtime.TotalSeconds > 60)
-        {
-            GameObject newObject = Instantiate(spawn);
-            spawned = true;
-
-        }
+    void DeleteItem()
+    {
+        
     }
 }
