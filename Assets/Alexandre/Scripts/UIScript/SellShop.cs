a113a20b96cb0ec80e7a0d4be112b466945095d2
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2024-03-04 13:45:02 +0100

    start economy

---
+++ Assets/Alexandre/Scripts/UIScript/SellShop.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Properties;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SellShop : MonoBehaviour
+{
+    public int money;
+    public List<SeedElements> aniseeds;
+    public GameObject unlockText;
+    public GameObject enoughText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SellSeed(SeedElements seed)
+    {
+        if (seed.seedAmount >= 1 && !seed.isLocked)
+        {
+            money += seed.intValue;
+            seed.seedAmount -= 1;
+            seed.amountSold += 1;
+
+            if (seed.amountSold >=10) 
+            {
+                seed.intValue -= 1;
+                seed.amountSold =0;
+            }
+        }
+
+        else if (seed.isLocked)
+        {
+            StartCoroutine(ShowLockText());
+        }
+
+        else if(seed.seedAmount <= 0)
+        {
+            StartCoroutine(ShowEnoughText());
+        }
+
+    }
+
+    private IEnumerator ShowLockText()
+    {
+        unlockText.SetActive(true);
+        yield return new WaitForSeconds(1);
+        unlockText.SetActive(false);
+    }
+
+    private IEnumerator ShowEnoughText()
+    {
+        enoughText.SetActive(true);
+        yield return new WaitForSeconds(1);
+        enoughText.SetActive(false);
+    }
+}
